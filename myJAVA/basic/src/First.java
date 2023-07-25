af51102d423f1834d69af4e0fc1ac1262341c5ea
sengna <jeonseino_o@naver.com>
2023-07-25 16:19:17 +0900

    KOSA

--- myJAVA/basic/src/First.java
+++
@@ -1,7 +0,0 @@
-public class First {
-    public static void main(String[] args) {
-        System.out.println("WELCOME!");
-        System.out.print("셍나");
-        System.out.print("짱:-)");
-    }
-}
