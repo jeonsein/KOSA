94f43dd0714f719ed5ff77a092c96a3c898b6ddd
sengna <jeonseino_o@naver.com>
2023-07-27 11:30:06 +0900

    KOSA 교육 과정 공부 내역

---
+++ myJAVA/oop/src/First.java
@@ -0,0 +1,11 @@
+
+public class First {
+	
+	static int smf; // static 필드
+	int mf;			// non-static 필드
+
+	public static void main(String[] args) {
+		int lv; // 지역변수(local variable)
+	} // end main
+
+} // end class
