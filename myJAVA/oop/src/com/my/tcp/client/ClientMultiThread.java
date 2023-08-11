f2cc93f74aabe40d04239d7de9c9385df7f7b4df
sengna <jeonseino_o@naver.com>
2023-08-11 09:19:18 +0900

    2023-08-11

--- myJAVA/oop/src/com/my/tcp/client/ClientMultiThread.java
+++ myJAVA/oop/src/com/my/tcp/client/ClientMultiThread.java
@@ -1,41 +1,120 @@
 package com.my.tcp.client;
+
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.ServerSocket;
+import java.net.ConnectException;
 import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
-class test extends Thread {
+
+// 데이터 수신용 Thread
+class ClientThread extends Thread {
 	
+	private Socket s;
+	InputStream is = null;
+	DataInputStream dis = null;
 	
+	ClientThread(Socket s) throws IOException {
+		this.s = s;
+		
+		is = s.getInputStream();
+		dis = new DataInputStream(is);
+	} // constructor
+	
+	// 목적: 무한반복 돌면서 수신받기
+	@Override
+	public void run() {
+		
+		// #1 Eclipse가 추천해준 try-catch 코드
+		// 소켓은 한번 망가졌으면 사용 불가능한데,
+//		while(true) {
+//			String receiveMsg;
+//			try {
+//				receiveMsg = dis.readUTF();
+//				System.out.println("서버가 되돌려준 메시지: " + receiveMsg);
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			} // try-catch
+//		}// while
+		
+		// 🔽🔽🔽🔽🔽🔽 직접 수정하기
+
+		// #2 직접 수정한 코드
+		try {
+			while(true) {
+				String receiveMsg;
+				receiveMsg = dis.readUTF();
+				
+				System.out.println("서버가 되돌려준 메시지: " + receiveMsg);
+			} // while
+		} catch (SocketException e) {
+		} catch (EOFException e) {
+		} catch (IOException e) {
+			e.printStackTrace();
+		} // try-catch
+		
+	} // run()
 	
 } // end class
 
-public class ClientMultiThread {
+// ---------------------------------------------------
 
-	public static void main(String[] args) {
+public class ClientMultiThread {
 	
-		Scanner sc = new Scanner(System.in);
-		
+	// 메인 Thread
+	public static void main(String[] args) throws IOException {
+	
 		// Socket 생성
 		Socket s = null;
-		ServerSocket ss = null;
-
-		s = new Socket();
-		ss = new ServerSocket();
+		String serverIP = "127.0.0.1";
+		int serverPort = 5432;
 		
 		OutputStream os = null;
 		DataOutputStream dos = null;
 		
-		// 스레드 
-		Thread t = new Thread() {
-			// 작업 스레드
-			@Override
-			public void run() {
-				
-			}
-		}
+		Scanner sc = new Scanner(System.in);
 		
+		try {
+			s = new Socket(serverIP, serverPort);
+			System.out.println("서버와 연결 성공");
+			
+			new ClientThread(s).start(); //새로운 스레드 시작
+			
+			os = s.getOutputStream();
+			dos = new DataOutputStream(os);
+
+			String sendMsg;
+			
+			do {
+				System.out.print("서버로 보낼 메시지(종료하려면 quit을 입력하세요.)");
+				
+				sendMsg = sc.nextLine();
+				dos.writeUTF(sendMsg);
+			} while(!sendMsg.equals("quit")); // do-while
+			
+		} catch (UnknownHostException e) {
+			System.out.println(serverIP +"서버는 존재하지 않습니다. 서버IP를 확인하세요 ");
+		} catch(ConnectException e) { //자식예외
+			System.out.println("서버가 실행되지 않았습니다. 서버실행을 확인하세요");
+		} catch(SocketException e) { //부모예외
+			System.out.println("서버가 강제종료되었습니다. 서버확인하세요");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			//dos.close(); -> oos.close() -> s.close();
+			if(s != null) {
+				try {
+					s.close();
+				} catch (IOException e) {} // try-catch
+			} // if
+		} // try-catch-finally
+	
 	} // end main
 	
 } // end class
--- myJAVA/oop/src/com/my/tcp/server/ServerForClientMultiThread.java
+++ myJAVA/oop/src/com/my/tcp/server/ServerForClientMultiThread.java
@@ -22,56 +22,62 @@ class ReceiveForClientMultiThread extends Thread{
 		this.s = s;
 		is = s.getInputStream();
 		dis = new DataInputStream(is);
-	}
+	} // constructor
 	
 	@Override
 	public void run() {
 		try {
 			String receiveMsg;
-			while(  !(receiveMsg = dis.readUTF()).equals("quit")           ) {
+			
+			while(  !(receiveMsg = dis.readUTF()).equals("quit")) {
 				System.out.println("클라이언트가 보낸 메시지:" + receiveMsg);
-			}
+			} // while
+			
 		} catch (IOException e) {
 		} finally {
 			System.out.println("클라이언트와의 연결이 종료되었습니다");
 			if(s != null) {
 				try {
 					s.close();
-				} catch (IOException e) {
-				}
-			}
-		}
-	}
-}
+				} catch (IOException e) {} // try-catch
+			} // if
+		} // try-catch-finally
+		
+	} // run()
+	
+} // end class
+
+//---------------------------------------------------
 
 class SendForClientMultiThread extends Thread{
 	
 	private Socket s;
-	private OutputStream oos = null;
+	private OutputStream os = null;
 	private DataOutputStream dos = null;
 	
 	SendForClientMultiThread(Socket s) throws IOException {
 		this.s = s;
-		oos = s.getOutputStream();
-		dos = new DataOutputStream(oos);
-	}
+		os = s.getOutputStream();
+		dos = new DataOutputStream(os);
+	} // constructor
 	
 	@Override
 	public void run() {
 		try {
 			for(int i=0; i<100; i++) {
 				dos.writeUTF("fromServerMsg-" + i);
+				
 				try {
 					Thread.sleep(200);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-}
+				} catch (InterruptedException e) { e.printStackTrace(); } // try-catch
+			} // for
+		} catch (IOException e) { e.printStackTrace(); } // try-catch
+	
+	} // run()
+	
+} // end class
+
+//---------------------------------------------------
 
 public class ServerForClientMultiThread {
 	
@@ -84,27 +90,25 @@ public class ServerForClientMultiThread {
 		
 		try {
 			ss = new ServerSocket(port);
+			
 			while(true) {
 				try {
 					System.out.println("클라이언트 접속을 기다리기");
+					
 					s = ss.accept();
+					
 					//수신용스레드
 					new ReceiveForClientMultiThread(s).start();
 					//송신용스레드
 					new SendForClientMultiThread(s).start();
-					
 				} catch(EOFException e) {
-				
-				} catch (IOException e) {
-					e.printStackTrace();
-				} 
-			}//end while
+				} catch (IOException e) { e.printStackTrace(); }  // try-catch
+			}// while
+			
 		} catch(BindException e) {
 			System.out.println(port +"포트가 이미 사용중입니다");
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}	
+		} catch (IOException e1) { e1.printStackTrace(); } // try-catch
 
-	}
+	} // end main
 
-}
+} // end class
