c209b56fb1717564a9992da330cdfcb4c28f6782
sengna <jeonseino_o@naver.com>
2023-09-05 11:21:20 +0900

    2023-09-05

---
+++ myFRONT/js/signup.js
@@ -0,0 +1,69 @@
+window.addEventListener('load', function(){
+
+    /*
+        중복확인 버튼 클릭되었을 때, 입력된 아이디 = 'admin' -> 이미 사용중인 아이디입니다.
+        중복확인 버튼 클릭되었을 때, 입력된 아이디 != 'admin' -> 사용 가능한 아이디입니다. 
+    */
+
+    // DOM Tree에서 id 입력란 객체 찾기
+    const inputIdObj = document.querySelector('#id'); // ('input[name="id"]'), ('#id'), ('form.signup>input[name=id]')
+    
+	/*
+        아이디 입력란에 포커스되었을 때, 가입하기 버튼 사라짐
+    */
+	// ------------ id 입력란 객체에서 focus 이벤트가 발생했을 때 할 일 START ------------
+	inputIdObj.addEventListener('focus', function(){
+		signupBtnObj.style.display = 'none'
+	}) // inputIdObj.addEventListener()
+	// ------------ id 입력란 객체에서 focus 이벤트가 발생했을 때 할 일 END ------------
+	
+	// DOM Tree에서 중복확인 버튼 객체 찾기
+    const duplicateBtnObj = document.querySelector('button.idDuplicateCheckBtn') // (form.signup>button[type=button])
+    // DOT Tree에서 가입하기 버튼 객체 찾기
+    const signupBtnObj = document.querySelector('button.signupBtn')
+
+   // ------------ button 객체에서 클릭 이벤트가 발생했을 때 할 일 START ------------
+    duplicateBtnObj.addEventListener('click', function(){
+        if(inputIdObj.value == 'admin') {
+            alert('이미 사용중인 아이디입니다.')
+        } else {
+            alert('사용 가능한 아이디입니다.')
+            signupBtnObj.style.display = 'inline-block'
+        } // if-else
+    }); // btnObj.addEventListener()    
+    // ------------ button 객체에서 클릭 이벤트가 발생했을 때 할 일 END ------------
+
+// --------------------------------------------------------------------------------------------    
+
+    /*
+        가입하기 버튼 클릭되었을 때, 비밀번호 값과 비밀번호1 값이 같을 경우 전송,
+        다를 경우 '비밀번호를 다시 입력하세요' 경고창 보여주기
+    */
+
+    // DOM Tree에서 폼객체 찾기
+    const formObj = document.querySelector('form.signup')
+
+	// DOM Tree에서 비밀번호 객체 찾기
+	const pwd1 = document.querySelector('#p');
+	const pwd2 = document.querySelector('#p1');
+
+    // ------------ 폼객체에서 submit 이벤트가 발생했을 때 할 일 START ------------
+    formObj.addEventListener('submit', function(e){
+		
+		// 동작 XXX
+        // const pwdArr = document.querySelectorAll('form.signup > input[type=password]')
+        // if(pwdArr[0].value != pwdArr[1].value) {
+        //     alert('비밀번호를 다시 입력하세요.')
+        //     e.preventDefault(); // 기본 이벤트 막기!
+        // } // if
+
+		// 비밀번호 객체 따로 받아와서 비교하기
+		if(pwd1.value !== pwd2.value) {
+			alert('비밀번호를 다시 입력하세요.');
+			e.preventDefault(); // 기본 이벤트 막기!
+		}; // if
+
+    }); // formObj.addEventListener()
+    // ------------ 폼객체에서 submit 이벤트가 발생했을 때 할 일 END ------------
+
+}); // window.addEventListener()
