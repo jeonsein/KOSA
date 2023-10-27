de9226737d90fcc194f653b0f8c33c98124678e3
sengna <jeonseino_o@naver.com>
2023-10-27 16:00:49 +0900

    [2023-10-27] 스프링부트 설정 방식 외부파일로 변경 + JPA

--- myJAVA/springbootJPA2/src/main/java/com/my/jpa/config/MyApplicationContext.java
+++ myJAVA/springbootJPA2/src/main/java/com/my/jpa/config/MyApplicationContext.java
@@ -1,7 +1,5 @@
-package com.my.board.config;
+package com.my.jpa.config;
 
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -9,8 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -28,25 +24,6 @@ public class MyApplicationContext {
 	@Autowired
 	Environment env;
 	
-	/*
-	@Bean
-	public HikariConfig hikariConfig() {
-		HikariConfig config = new HikariConfig();
-		
-//		config.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
-		config.setDriverClassName(hikariDriverClassName);
-		
-		config.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@localhost:1521:xe");
-//		config.setUsername("hr");
-		config.setUsername(env.getProperty("spring.datasource.hikari.username"));
-		config.setPassword("hr");
-		config.setMinimumIdle(3);
-		
-		return config;
-		
-	} // hikariconfig()
-	*/
-	
 	@Bean
 	@ConfigurationProperties("spring.datasource.hikari")
 	public HikariConfig hikariConfig() {
@@ -59,18 +36,6 @@ public class MyApplicationContext {
 		return new HikariDataSource(hikariConfig());
 	} // dataSourceHikari()
 	
-	@Bean
-	public SqlSessionFactory sqlSessionFactory() throws Exception {
-		SqlSessionFactoryBean ssfb = new SqlSessionFactoryBean();
-		
-		ssfb.setDataSource(dataSourceHikari());
-		Resource resource = new ClassPathResource("mybatis-config.xml");
-		
-		ssfb.setConfigLocation(resource);
-		
-		return (SqlSessionFactory)ssfb.getObject();
-		
-	} // sqlSessionFactory()
 	
 	@Bean
 	public DataSourceTransactionManager transactionManager() {
--- myJAVA/springboot_board/src/main/java/com/my/board/dto/Board.java
+++ myJAVA/springboot_board/src/main/java/com/my/board/dto/Board.java
@@ -3,6 +3,9 @@ package com.my.board.dto;
 import java.util.Date;
 import java.util.List;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,9 +18,16 @@ import lombok.ToString;
 public class Board {
 
 	private Integer boardNo;
+	
 	private String boardTitle;
+	
+	@NotEmpty(message = "글 내용은 반드시 입력하세요")
+	@Size(max = 10, message = "글 내용은 최대 10자리까지만 가능합니다")
 	private String boardContent;
+	
+	@NotEmpty(message = "글 작성자 아이디는 반드시 입력하세요")
 	private String boardId;
+	
 	private Date boardDt;
 	
 	// 게시글 하나에 답글은 여러개일 수 있음
