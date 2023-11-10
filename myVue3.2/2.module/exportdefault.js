ce4bc0911f04f5e9b4e9b7c5d53443fac163809a
sengna <jeonseino_o@naver.com>
2023-11-10 12:43:56 +0900

    [2023-11-10] Vue.js 문법 공부 및 실습_todolist 만들기

--- myVue3.2/2.module/exportdefault.js
+++ myVue3.2/2.module/exportdefault.js
@@ -1,7 +1,16 @@
+// 2023-11-09
+// const base = 100
+// const calculator = {
+//     add : (x) => base+ x,
+//     multiply :  (x) => base * x,
+//     test : ()=> console.log('module내부에서 this는 undefined입니다', this)
+// }
+// export default calculator
+
+// 2023-11-10
 const base = 100
-const calculator = {
+export default  {
     add : (x) => base+ x,
     multiply :  (x) => base * x,
     test : ()=> console.log('module내부에서 this는 undefined입니다', this)
 }
-export default calculator
